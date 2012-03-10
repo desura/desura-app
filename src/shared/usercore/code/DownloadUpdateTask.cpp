ac21abd00bf614defab6b857e220dc44cb9269d8
Jookia <166291@gmail.com>
2012-03-11 01:30:08 +1100

    Now using shared TinyXML!

--- src/shared/usercore/code/DownloadUpdateTask.cpp
+++ src/shared/usercore/code/DownloadUpdateTask.cpp
@@ -101,7 +101,7 @@ void DownloadUpdateTask::downloadUpdate()
 
 
 	TiXmlDocument doc;
-	doc.LoadBuffer(const_cast<char*>(wc->getData()), wc->getDataSize());
+	XML::loadBuffer(doc, const_cast<char*>(wc->getData()), wc->getDataSize());
 
 	XML::processStatus(doc, "appupdate");
 
