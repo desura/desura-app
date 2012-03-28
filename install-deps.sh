480cfa750980ca657267c2fed5e6b2b8f0238c87
Karol Herbst <git@karolherbst.de>
2012-03-28 22:22:02 +0200

    fixes https://github.com/lodle/Desurium/issues/114

--- install-deps.sh
+++ install-deps.sh
@@ -1,44 +1,40 @@
 #!/bin/sh
-# Determine the Linux distribution that is being run and install the build dependencies for it.
+# Determine the operating system distribution that is being run and install the build dependencies for it.
 
 if [ "$(whoami)" != "root" ]; then
 	echo "Run this as root! sudo should do the trick."
 	exit
-
-echo "NOTE: It'd be a good idea in general to read the contents of this script if you haven't already."
-# Sounds scary, just to catch attention. Thanks for reading!
-
-PASS_MESSAGE="Please provide root password to install build dependecies"
-DISTRIBUTION="Unknown"
-UNSUPPORTED_DISTROS="Unknown distribution."
-UNSUPPORTED_MESSAGE=" is not supported yet, consult this script and figure out dependencies for it."
-
-if [ -f /etc/SuSE-release ]; then
-	DISTRIBUTION="SUSE"
-elif [ -f /etc/debian_version ]; then
-	DISTRIBUTION="Debian"
-	if [ "$(whoami)" != 'root' ]; then
-		echo $PASS_MESSAGE
-	fi
-	su -c 'apt-get install gcc git-core subversion m4 build-essential binutils automake autoconf libtool libgtk2.0-dev libnss3-dev libgconf2-dev libgnome-keyring-dev libdbus-glib-1-dev gperf bison libcups2-dev flex libjpeg-dev libasound2-dev libbz2-dev libxpm-dev libx11-dev libssl-dev libnotify-dev scons libv8-dev libc-ares-dev libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-system-dev libboost-thread-dev'
-elif [ -f /etc/redhat-release ]; then
-	DISTRIBUTION="Red Hat"
-	if [ "$(whoami)" != 'root' ]; then
-		echo $PASS_MESSAGE
-	fi
-	su -c 'yum install git subversion m4 autoconf gcc-c++ glibc-devel binutils autoconf libtool gtk2-devel nss-devel GConf2-devel libgnome-keyring-devel dbus-glib-devel gperf bison cups-devel flex libjpeg-turbo-devel alsa-lib-devel bzip2-devel libXpm-devel libX11-devel openssl-devel libnotify-devel scons'
-elif [ -f /etc/arch-release ]; then
-	DISTRIBUTION="Arch"
-	if [ "$(whoami)" != 'root' ]; then
-		echo $PASS_MESSAGE
-	fi
-	DEPS="$(pacman -T git subversion m4 autoconf gcc glibc binutils autoconf libtool gtk2 nss libgnome-keyring dbus-glib gperf bison cups flex libjpeg-turbo alsa-lib bzip2 libxpm libx11 openssl scons gconf libnotify boost c-ares v8 bzip2 tinyxml sqlite)"
-	su -c 'pacman -S $DEPS'
-elif [ -f /etc/slackware-version ]; then
-	DISTRIBUTION="Slackware"
 fi
 
-echo $UNSUPPORTED_DISTROS| grep -q $DISTRIBUTION
-if [ $? -eq 0 ];then
-  echo $DISTRIBUTION $UNSUPPORTED_MESSAGE
+# use dpkg -l instead
+if [ -f /etc/debian_version ]; then # Debian  (untested!)
+    DEPS="gcc git-core subversion m4 build-essential binutils automake autoconf libtool libgtk2.0-dev libnss3-dev libgconf2-dev libgnome-keyring-dev libdbus-glib-1-dev gperf bison libcups2-dev flex libasound2-dev libbz2-dev libxpm-dev libx11-dev libssl-dev libnotify-dev scons libv8-dev libc-ares-dev libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-system-dev libboost-thread-dev"
+    ST=`dpkg -l ${DEPS} |& awk /^ii\ /  | awk '{print $2}'`
+    for PACK in ${DEPS}
+    do
+        CHECK=`echo "${ST}" | grep ${PACK}`
+        if [ "${CHECK}" == "" ] ; then
+            MUST_BE_INSTALLED="${MUST_BE_INSTALLED} ${PACK}"
+        fi
+    done
+    if [ "${MUST_BE_INSTALLED}" != "" ] ; then
+        aptitude install ${MUST_BE_INSTALLED}
+    else
+        echo "everything intalled"
+    fi
+elif [ -f /etc/redhat-release ]; then
+	yum install git subversion m4 autoconf gcc-c++ libstdc++-static glibc-devel binutils autoconf libtool gtk2-devel nss-devel GConf2-devel libgnome-keyring-devel dbus-glib-devel gperf bison cups-devel flex libjpeg-turbo-devel alsa-lib-devel bzip2-devel libXpm-devel libX11-devel openssl-devel libnotify-devel scons xdg-user-dirs
+elif [ -f /etc/arch-release ]; then
+	# By using "pacman -T" to find out needed dependencies, we don't get
+	# conflicts if a package we have installed provides one of the dependencies.
+	DEPS=`pacman -T git subversion m4 autoconf gcc glibc binutils autoconf libtool gtk2 nss libgnome-keyring dbus-glib gperf bison cups flex libjpeg-turbo alsa-lib bzip2 libxpm libx11 openssl scons gconf libnotify xdg-user-dirs v8 | sed -e 's/\n/ /g'`
+	if [ -z "${DEPS}" ]; then
+		echo "Dependencies already installed."
+	else
+		pacman -S --asdeps ${DEPS}
+	fi
+elif [ -f /usr/bin/emerge ]; then # Gentoo
+    echo "Please copy the ebuild under gentoo/portage into a local overlay and emerge via emerge --onlydeps desura"
+else # else
+	echo "Unsupported operating system."
 fi
