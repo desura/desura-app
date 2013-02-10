9acd58c3a94e6f99a64e351f5c3e2d2c23221d53
Karol Herbst <git@karolherbst.de>
2013-02-10 12:56:44 +0100

    cmake: add -Wl,--as-needed linker flag

--- cmake/platform/unix/compiler/Clang.cmake
+++ cmake/platform/unix/compiler/Clang.cmake
@@ -1,5 +1,12 @@
 message("-- using clang")
 add_compiler_flags(-fPIC -pipe -fvisibility=hidden -finline-functions)
-add_compiler_flags(DEBUG -rdynamic -fno-omit-frame-pointer -g3)
+add_compiler_flags(DEBUG -fno-omit-frame-pointer -g3)
 add_compiler_flags(RELEASE -O2)
 add_compiler_flags(CXX -std=c++0x)
+add_linker_flags(-Wl,--as-needed)
+
+if(DEBUG)
+  add_linker_flags(-rdynamic)
+endif()
+
+set(CLANG TRUE)
