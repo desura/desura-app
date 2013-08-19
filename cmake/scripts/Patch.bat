f7967dd4629b92f24c6d9e7af21b99649295bd1d
Karol Herbst <git@karolherbst.de>
2013-08-19 17:08:22 +0200

    cmake/patch: silence win32 patch script a bit

--- cmake/scripts/Patch.bat
+++ cmake/scripts/Patch.bat
@@ -1,6 +1,4 @@
-patch -p0 -N -i %1
-echo %errorlevel% Error level
-
+"@PATCH_EXECUTABLE@" -p0 -N -i %1
 REM if we hit one means the patch is all ready applied
 IF ["%errorlevel%"]==["1"] (
 	SET errorlevel=0
