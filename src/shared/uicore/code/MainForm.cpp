desura/Desurium

Fixed compile errors on Windows Updated about page to have gpl info #26

src/shared/uicore/code/MainForm.cpp
@@ -91,7 +91,7 @@ static CVar gc_allow_wm_positioning("gc_allow_wm_positioning", "true");
 #endif
 
 #ifdef NIX64
-MainForm::MainForm(wxWindow* parent, bool offline) : gcFrame(parent, wxID_ANY, wxT(PRODUCT_NAME " 64"), wxDefaultPosition, wxSize(990, 690), wxDEFAULT_FRAME_STYLE, true )
+MainForm::MainForm(wxWindow* parent, bool offline) : gcFrame(parent, wxID_ANY, PRODUCT_NAME_CATW(L" 64"), wxDefaultPosition, wxSize(990, 690), wxDEFAULT_FRAME_STYLE, true )
 #else
 MainForm::MainForm(wxWindow* parent, bool offline) : gcFrame(parent, wxID_ANY, wxT(PRODUCT_NAME), wxDefaultPosition, wxSize(990, 690), wxDEFAULT_FRAME_STYLE, true )
 #endif

src/shared/uicore/code/UICoreMain.cpp
@@ -446,7 +446,7 @@ public:
 		}
 		else
 		{
-			wxMessageBox(wxT("Another instance is all ready running."), wxT(PRODUCT_NAME " Error"));
+			wxMessageBox(wxT("Another instance is all ready running."), PRODUCT_NAME_CATW(L" Error"));
 			return false;
 		}
 #endif

src/shared/utilcore/code/McfViewerForm.cpp
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 #include "../../../branding/branding.h"
 
-McfViewerForm::McfViewerForm(wxWindow* parent, gcString mcf) : gcFrame(parent, wxID_ANY, wxT(PRODUCT_NAME ": Mcf Viewer"), wxDefaultPosition, wxSize(370,500), wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL)
+McfViewerForm::McfViewerForm(wxWindow* parent, gcString mcf) : gcFrame(parent, wxID_ANY, PRODUCT_NAME_CATW(L": Mcf Viewer"), wxDefaultPosition, wxSize(370,500), wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL)
 {
 	Bind(wxEVT_COMMAND_BUTTON_CLICKED, &McfViewerForm::onButtonClicked, this); 
 	Bind(wxEVT_CLOSE_WINDOW, &McfViewerForm::onClose, this);

src/shared/utilcore/code/ToolInstallForm.cpp
@@ -39,7 +39,7 @@ void PipeIsActive()
 		g_pToolInstallForm->pipeIsActive();
 }
 
-ToolInstallForm::ToolInstallForm(wxWindow* parent, const char* key) : gcFrame(parent, wxID_ANY, wxT(PRODUCT_NAME " Tool Install Helper"), wxDefaultPosition, wxSize(354,82), wxCAPTION|wxCLOSE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL|wxMINIMIZE_BOX)
+ToolInstallForm::ToolInstallForm(wxWindow* parent, const char* key) : gcFrame(parent, wxID_ANY, PRODUCT_NAME_CATW(L" Tool Install Helper"), wxDefaultPosition, wxSize(354,82), wxCAPTION|wxCLOSE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL|wxMINIMIZE_BOX)
 {
 	m_szKey = key;
 	g_pToolInstallForm = this;

src/shared/utilcore/code/UninstallDesuraForm.cpp
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 void SetExitCode(int32 code);
 
-UninstallForm::UninstallForm(wxWindow* parent, UserCore::UserI* user) : wxFrame(parent, wxID_ANY, wxT(PRODUCT_NAME " UnInstaller"), wxDefaultPosition, wxSize( 500,330 ), wxCAPTION|wxMINIMIZE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL )
+UninstallForm::UninstallForm(wxWindow* parent, UserCore::UserI* user) : wxFrame(parent, wxID_ANY, PRODUCT_NAME_CATW(L" UnInstaller"), wxDefaultPosition, wxSize( 500,330 ), wxCAPTION|wxMINIMIZE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL )
 {
 	m_pUser = user;
 

desura/desura-app

src/shared/uicore/code/MainForm.cpp
@@ -91,9 +91,9 @@ static CVar gc_allow_wm_positioning("gc_allow_wm_positioning", "true");
 #endif
 
 #ifdef NIX64
-MainForm::MainForm(wxWindow* parent, bool offline) : gcFrame(parent, wxID_ANY, wxT("Desura 64"), wxDefaultPosition, wxSize(990, 690), wxDEFAULT_FRAME_STYLE, true )
+MainForm::MainForm(wxWindow* parent, bool offline) : gcFrame(parent, wxID_ANY, wxT(PRODUCT_NAME " 64"), wxDefaultPosition, wxSize(990, 690), wxDEFAULT_FRAME_STYLE, true )
 #else
-MainForm::MainForm(wxWindow* parent, bool offline) : gcFrame(parent, wxID_ANY, wxT("Desura"), wxDefaultPosition, wxSize(990, 690), wxDEFAULT_FRAME_STYLE, true )
+MainForm::MainForm(wxWindow* parent, bool offline) : gcFrame(parent, wxID_ANY, wxT(PRODUCT_NAME), wxDefaultPosition, wxSize(990, 690), wxDEFAULT_FRAME_STYLE, true )
 #endif
 {
 	Bind(wxEVT_COMMAND_MENU_SELECTED, &MainForm::onMenuSelect, this);

src/shared/uicore/code/UICoreMain.cpp
@@ -444,7 +444,7 @@ public:
 		}
 		else
 		{
-			wxMessageBox(wxT("Another instance is all ready running."), wxT("Desura Error"));
+			wxMessageBox(wxT("Another instance is all ready running."), wxT(PRODUCT_NAME " Error"));
 			return false;
 		}
 #endif

src/shared/utilcore/code/McfViewerForm.cpp
@@ -24,7 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 #include "mcfcore/MCFHeaderI.h"
 #include "mcfcore/MCFFileI.h"
 
-McfViewerForm::McfViewerForm(wxWindow* parent, gcString mcf) : gcFrame(parent, wxID_ANY, wxT("Desura: Mcf Viewer"), wxDefaultPosition, wxSize(370,500), wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL)
+#include "../../../branding/branding.h"
+
+McfViewerForm::McfViewerForm(wxWindow* parent, gcString mcf) : gcFrame(parent, wxID_ANY, wxT(PRODUCT_NAME ": Mcf Viewer"), wxDefaultPosition, wxSize(370,500), wxDEFAULT_FRAME_STYLE|wxTAB_TRAVERSAL)
 {
 	Bind(wxEVT_COMMAND_BUTTON_CLICKED, &McfViewerForm::onButtonClicked, this); 
 	Bind(wxEVT_CLOSE_WINDOW, &McfViewerForm::onClose, this);

src/shared/utilcore/code/ToolInstallForm.cpp
@@ -37,7 +37,7 @@ void PipeIsActive()
 		g_pToolInstallForm->pipeIsActive();
 }
 
-ToolInstallForm::ToolInstallForm(wxWindow* parent, const char* key) : gcFrame(parent, wxID_ANY, wxT("Desura Tool Install Helper"), wxDefaultPosition, wxSize(354,82), wxCAPTION|wxCLOSE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL|wxMINIMIZE_BOX)
+ToolInstallForm::ToolInstallForm(wxWindow* parent, const char* key) : gcFrame(parent, wxID_ANY, wxT(PRODUCT_NAME " Tool Install Helper"), wxDefaultPosition, wxSize(354,82), wxCAPTION|wxCLOSE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL|wxMINIMIZE_BOX)
 {
 	m_szKey = key;
 	g_pToolInstallForm = this;

src/shared/utilcore/code/UninstallDesuraForm.cpp
@@ -23,9 +23,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 #include "UDFProgressPanel.h"
 #include "usercore\UserCoreI.h"
 
+#include "../../../branding/branding.h"
+
 void SetExitCode(int32 code);
 
-UninstallForm::UninstallForm(wxWindow* parent, UserCore::UserI* user) : wxFrame(parent, wxID_ANY, wxT("Desura UnInstaller"), wxDefaultPosition, wxSize( 500,330 ), wxCAPTION|wxMINIMIZE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL )
+UninstallForm::UninstallForm(wxWindow* parent, UserCore::UserI* user) : wxFrame(parent, wxID_ANY, wxT(PRODUCT_NAME " UnInstaller"), wxDefaultPosition, wxSize( 500,330 ), wxCAPTION|wxMINIMIZE_BOX|wxSYSTEM_MENU|wxTAB_TRAVERSAL )
 {
 	m_pUser = user;
 