desura/Desurium

Desurium -> Desura (outside of branding)

run_desura.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=`pwd`/install/lib
-install/bin/Desurium
+install/bin/Desura

desura/desura-app

run_desura.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=`pwd`/install/lib
-cd install/bin
+install/bin/Desurium
 ./Desurium