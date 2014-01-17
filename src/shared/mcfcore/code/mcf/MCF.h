7dec5907df841e8d4f38f873128014a66872d548
Mark Chandler <mark@moddb.com>
2014-01-17 08:25:12 -0800

    Changed add file in mcf to use move semantics to avoid extra copy and compiler issue

--- src/shared/mcfcore/code/mcf/MCF.h
+++ src/shared/mcfcore/code/mcf/MCF.h
@@ -83,7 +83,7 @@ public:
 	//! 
 	//! @param file MCFFile to add
 	//!
-	void addFile(std::shared_ptr<MCFCore::MCFFile>& file);
+	void addFile(std::shared_ptr<MCFCore::MCFFile>&& file);
 
 	//! Finds a files index by its hash
 	//!
