788a539cf4ca30dc4a2bd450c1bff45e4511bd9f
Mark Chandler <mark@moddb.com>
2014-02-05 09:33:19 -0800

    Changed zeroBuffer to use memset and added tests to make sure it does what its meant to do

--- src/shared/unittests/code/util_string/UtilString.cpp
+++ src/shared/unittests/code/util_string/UtilString.cpp
@@ -24,5 +24,60 @@ namespace UnitTest
 
 		ASSERT_STREQ("HelloImAString! ", strRes.c_str());
 	}
+
+
+	class UtilStringP : public ::testing::TestWithParam<std::pair<int, int>>
+	{
+	public:
+
+	};
+
+	INSTANTIATE_TEST_CASE_P(zeroBuffer_char,
+		UtilStringP,
+		::testing::Values(std::make_pair(0, 10), std::make_pair(0, 5), std::make_pair(5, 10),
+		std::make_pair(2, 8)));
+
+	INSTANTIATE_TEST_CASE_P(zeroBuffer_wchar,
+		UtilStringP,
+		::testing::Values(std::make_pair(0, 10), std::make_pair(0, 5), std::make_pair(5, 10),
+		std::make_pair(2, 8)));
+
+	TEST_P(UtilStringP, zeroBuffer_char)
+	{
+		char szTest[10] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+		auto off = GetParam().first;
+		auto end = GetParam().second;
+
+		UTIL::STRING::zeroBuffer(&szTest[off], end - off);
+
+		for (int x = 0; x < off; x++)
+			ASSERT_EQ((char)0xFF, szTest[x]);
+
+		for (int x=off; x<end; x++)
+			ASSERT_EQ((char)0, szTest[x]);
+
+		for (int x = end; x < 10; x++)
+			ASSERT_EQ((char)0xFF, szTest[x]);
+	}
+
+	TEST_P(UtilStringP, zeroBuffer_wchar)
+	{
+		wchar_t szTest[10] = {0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF};
+
+		auto off = GetParam().first;
+		auto end = GetParam().second;
+
+		UTIL::STRING::zeroBuffer(&szTest[off], end - off);
+
+		for (int x = 0; x < off; x++)
+			ASSERT_EQ((wchar_t)0xFFFF, szTest[x]);
+
+		for (int x = off; x < end; x++)
+			ASSERT_EQ((wchar_t)0, szTest[x]);
+
+		for (int x = end; x < 10; x++)
+			ASSERT_EQ((wchar_t)0xFFFF, szTest[x]);
+	}
 }
 
