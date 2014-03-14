43c15ae9b6483c1f1b5b6bd00343841b546e411e
Lodle <mchandler@lindenlab.com>
2014-03-13 18:44:02 -0700

    Reduced debug output

--- src/shared/mcfcore/code/thread/SFTWorker.cpp
+++ src/shared/mcfcore/code/thread/SFTWorker.cpp
@@ -269,7 +269,6 @@ bool SFTWorker::newTask()
 	if (!m_pCurFile)
 		return false;
 
-	Debug(gcString("{0}: starting Decompressing file {1}.\n", m_uiId, m_pCurFile->getName()));
 	std::string file = m_pCurFile->getFullPath();
 
 	safe_delete(m_pBzs);
