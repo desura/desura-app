7b1f3e0a4bd85e2efcedc737da7b2bac0f37e050
Lodle <mchandler@lindenlab.com>
2014-03-20 14:25:44 -0700

    Fixed linux compile issues

--- src/include/SharedObjectLoader.h
+++ src/include/SharedObjectLoader.h
@@ -76,7 +76,8 @@ public:
 		m_bHasFailed = false;
 
 #ifdef NIX
-		m_hHandle = dlopen((UTIL::OS::getRuntimeLibPath() + convertToLinuxModule(module)).c_str(), RTLD_NOW);
+		gcString strModule(convertToLinuxModule(module));
+		m_hHandle = dlopen((UTIL::OS::getRuntimeLibPath() + strModule).c_str(), RTLD_NOW);
 
 		if (!m_hHandle)
 			fprintf(stderr, "%s:%d - Error loading library %s: '%s' [LD_LIBRARY_PATH=%s]\n", __FILE__, __LINE__, strModule, dlerror(), getenv("LD_LIBRARY_PATH"));
