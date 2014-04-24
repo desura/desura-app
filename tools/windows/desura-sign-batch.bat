a4574b630b1953c5193312262ce24ef2d7bc7b8d
sabinlinden <sabin@lindenlab.com>
2014-04-24 15:11:46 -0700

    Moved signing back out to teamcity.

--- tools/windows/desura-sign-batch.bat
+++
@@ -1,6 +0,0 @@
-REM BINPATH path to exe's and bin files
-REM SIGNTOOL tool to use to sign exes and dlls
-REM CERTPATH cert to use to sign
-
-for %%i in (%BINPATH%\*.exe) do (%SIGNTOOL% sign -f %CERTPATH% -p lindenlab -t http://timestamp.comodoca.com/authenticode %%i)
-for %%i in (%BINPATH%\bin\*core.dll) do (%SIGNTOOL% sign -f %CERTPATH% -p lindenlab -t http://timestamp.comodoca.com/authenticode %%i)
