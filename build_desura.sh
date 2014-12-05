ba81954fc7e62ee388db7ec0ef3e6c9fd8df5bf0
Tom Thompson <tthompson@badjuju.com>
2014-12-04 17:05:47 -0800

    fix deb packaging

--- build_desura.sh
+++ build_desura.sh
@@ -9,6 +9,8 @@ clean() {
 	rm -rf build_cef
 	rm -rf install
 	rm -rf build_package
+	rm *.deb
+	rm *.rpm
 	echo 'Cleaned'
 }
 
@@ -44,7 +46,9 @@ pack() {
 	fi
 	cd build_package
 	cmake .. -DPACKAGE_TYPE=$PACKAGE -DINSTALL_DESKTOP_FILE=ON -DCMAKE_INSTALL_PREFIX="/opt/desura" -DDESKTOP_EXE="/opt/desura/desura" -DDESKTOP_ICON="/opt/desura/desura.png" || exit 1
-	make package $@
+	NUM_PROC=`nproc`
+	echo "${NUM_PROC} processors detected"
+	make -j${NUM_PROM} package $@
 	if [ $PACKAGE = "DEB" ]; then
 		mv Desura-*.deb ..
 	elif [ $PACKAGE = "RPM" ]; then
