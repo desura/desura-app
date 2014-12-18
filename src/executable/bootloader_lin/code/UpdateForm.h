9330cd92aa4354ec8a4b2dd41f33fd06cd753b88
Tom Thompson <tthompson@badjuju.com>
2014-12-18 10:14:29 -0800

    fix license headers

--- src/executable/bootloader_lin/code/UpdateForm.h
+++ src/executable/bootloader_lin/code/UpdateForm.h
@@ -1,6 +1,6 @@
 /*
-Desura is the leading indie game distribution platform
 Copyright (C) 2011 Mark Chandler (Desura Net Pty Ltd)
+Copyright (C) 2014 Bad Juju Games, Inc.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -13,9 +13,12 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>
-*/
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA.
 
+Contact us at legal@badjuju.com.
+
+*/
 #ifndef DESURA_UPDATE_FORM_NIX_H
 #define DESURA_UPDATE_FORM_NIX_H
 #ifdef _WIN32
